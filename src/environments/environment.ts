3ab563ff48cd2136ddc28aebc9c7eac7e90eaa4b
igorrc <igor.ramos.cruzado.w@gmail.com>
2024-10-23 07:40:13 -0500

    feat: add folders of the migratcion client Dar v2

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    URL_BACKEND: 'https://api.admincursos-udemy.site',
+    URL_SERVICIOS: 'https://api.admincursos-udemy.site/api',
+    URL_FRONTEND: 'https://admin.admincursos-udemy.site/'
+};
