d3a46a185987261a9599d32ace703f53d2d319c8
Novak Zecevich <novak.zecevich@e-capture.io>
2024-10-24 01:15:55 -0500

    feat: refactor login

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ComponentsRoutingModule } from './components-routing.module';
 import { ComponentsComponent } from './components.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 @NgModule({
   declarations: [
-    ComponentsComponent
+    ComponentsComponent,
+    CarouselComponent
+  ],
+  exports: [
+    CarouselComponent
   ],
   imports: [
     CommonModule,
