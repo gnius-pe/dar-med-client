0b7f85528991e4e04fdc6504a2015fea9b128b84
igorrc <igor.ramos.cruzado.w@gmail.com>
2024-12-06 12:39:17 -0500

    bugfix: compilaion susscefull

---
+++ environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    URL_BACKEND: 'https://api.v1.core.dar.reformaperu.org',
+    URL_SERVICIOS: 'https://api.v1.core.dar.reformaperu.org/api',
+    URL_FRONTEND: 'https://app.dar.reformaperu.org/'
+};
