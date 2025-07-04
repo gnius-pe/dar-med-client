65d3b92f3a66e8987c343097820e4ab1e8ff4632
novak <novakmzv@gmail.com>
2025-07-04 00:00:21 -0500

    feat: added notification component in login

--- src/app/authentication/login/login.module.ts
+++ src/app/authentication/login/login.module.ts
@@ -5,18 +5,20 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import {ComponentsModule} from "../../core/components/components.module";
+import {LoadingOverlayComponent} from "../../shared/components/loading-overlay/loading-overlay.component";
 
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule,
-    SharedModule,
-    NgOptimizedImage,
-    ComponentsModule
-  ]
+    imports: [
+        CommonModule,
+        LoginRoutingModule,
+        SharedModule,
+        NgOptimizedImage,
+        ComponentsModule,
+        LoadingOverlayComponent
+    ]
 })
 export class LoginModule { }
