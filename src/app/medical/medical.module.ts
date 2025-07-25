643003545cd95dde737c832878cc12dcf5200791
novak <novakmzv@gmail.com>
2025-07-25 18:40:09 -0500

    feat: modify MedicalModule

--- src/app/medical/medical.module.ts
+++ src/app/medical/medical.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MedicalRoutingModule } from './medical-routing.module';
 import { MedicalComponent } from './medical.component';
 import { SharedModule } from '../shared/shared.module';
+import { CreateDoctorTicketsModalComponent } from './tickets/create-doctor-tickets-modal/create-doctor-tickets-modal.component';
 
 @NgModule({
-  declarations: [
-    MedicalComponent,
-  ],
-  imports: [
-    CommonModule,
-    MedicalRoutingModule,
-    SharedModule,
-  ]
+    declarations: [
+        MedicalComponent,
+        CreateDoctorTicketsModalComponent,
+    ],
+    exports: [
+        CreateDoctorTicketsModalComponent
+    ],
+    imports: [
+        CommonModule,
+        MedicalRoutingModule,
+        SharedModule,
+    ]
 })
 export class MedicalModule { }
